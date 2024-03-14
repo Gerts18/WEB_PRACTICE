5f8c7e12dde96a57bb6d3ae249fa28ced18c3af0
German <germants100@gmail.com>
2024-03-13 22:44:10 -0600

    Choose a winner

--- REACT/TUTORIAL/src/components/Board/Board.jsx
+++ REACT/TUTORIAL/src/components/Board/Board.jsx
@@ -4,29 +4,64 @@ import '../style.css'
 
 
 const Board = () => {
+
+  function calculateWinner(squares){
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   
   const [xIsNext, setXIsNext] = useState(true)
 
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  const handleClick = (i) => {
-    if(squares[i]){
-      return;
-    }
+    const handleClick = (i) => {
+      if (squares[i] || calculateWinner(squares)) {
+        return;
+      }
 
-    const nextSquares = squares.slice();
 
-    if(xIsNext) {
-      nextSquares[i] = 'X';
-    } else{
-      nextSquares[i] = 'O';
-    }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+      if(squares[i]){
+        return;
+      }
+
+      const nextSquares = squares.slice();
+
+      if(xIsNext) {
+        nextSquares[i] = 'X';
+      } else{
+        nextSquares[i] = 'O';
+      }
+      setSquares(nextSquares);
+      setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Ganador: " + winner;
+  } else {
+    status = "Siguiente jugador: " + (xIsNext ? "X" : "O");
   }
 
   return (
     <>
+      <h1 className='status'>{status}</h1>
       <div className='board-row'>
         <Square value = {squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value = {squares[1]} onSquareClick={() => handleClick(1)}/>
