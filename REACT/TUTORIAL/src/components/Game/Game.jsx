bfd725ea6efc21a9ab2e666ddf42a6f9ea9424ea
German <germants100@gmail.com>
2024-03-14 09:10:58 -0600

    setting game component as the superior component

--- REACT/TUTORIAL/src/components/Game/Game.jsx
+++ REACT/TUTORIAL/src/components/Game/Game.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Board from '../Board/Board'
+import '../style.css'
 
 const Game = () => {
   return (
     <>
-         
+        <div className='game'>
+            <div className='game-board'>
+                <Board/>
+            </div>
+            <div className='game-info'>
+                <ol> {} </ol>
+            </div>
+        </div> 
     </>
   )
 }
