3703b07927f006b7ecf5ba3154879923e3a2a6ea
German <germants100@gmail.com>
2024-03-13 22:33:04 -0600

    adding the functionality to take turns

--- REACT/TUTORIAL/src/components/Board/Board.jsx
+++ REACT/TUTORIAL/src/components/Board/Board.jsx
@@ -4,13 +4,21 @@ import '../style.css'
 
 
 const Board = () => {
+  
+  const [xIsNext, setXIsNext] = useState(true)
 
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   const handleClick = (i) => {
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+
+    if(xIsNext) {
+      nextSquares[i] = 'X';
+    } else{
+      nextSquares[i] = 'O';
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
