0da14121e56def91f43b44465d375943031768c2
German <germants100@gmail.com>
2024-03-17 19:42:40 -0600

    Cleaning the code

--- REACT/TUTORIAL/src/components/Game/Game.jsx
+++ REACT/TUTORIAL/src/components/Game/Game.jsx
@@ -4,21 +4,20 @@ import '../style.css'
 
 const Game = () => {
 
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[currentMove];
     const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+    
 
     function handlePlay (nextSquares){
         const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
         setHistory(nextHistory);
         setCurrentMove(nextHistory.length - 1);
-        setXIsNext(!xIsNext);
     }
 
     function jumpTo(nextMove){
         setCurrentMove(nextMove);
-        setXIsNext(nextMove % 2 === 0)
     }
 
     const moves = history.map((squares, move) => {
