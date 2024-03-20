48c094f1bc635f2b95eda18819072d4960891b50
German <germants100@gmail.com>
2024-03-19 22:44:33 -0600

    Second challenge complete

--- REACT/TUTORIAL/src/components/Board/Board.jsx
+++ REACT/TUTORIAL/src/components/Board/Board.jsx
@@ -58,25 +58,32 @@ const Board = ({xIsNext, squares, onPlay}) => {
     status = "Siguiente jugador: " + (xIsNext ? "X" : "O");
   }
 
+  const allSquares = (num) => {
+    const s = [];
+    for (let i = num; i<(num+3); i++){
+      s.push(
+        <Square key={i} value = {squares[i]} onSquareClick={() => handleClick(i)} />
+      )
+    }
+
+    return <>{s}</>
+    
+  }
+
+
   return (
     <>
       <h1 className='status'>{status}</h1>
       <div className='board-row'>
-        <Square value = {squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value = {squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value = {squares[2]} onSquareClick={() => handleClick(2)}/>
+        {allSquares(0)}
       </div>
       
       <div className='board-row'>
-        <Square value = {squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value = {squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value = {squares[5]} onSquareClick={() => handleClick(5)}/>
+        {allSquares(3)}
       </div>
       
       <div className='board-row'>
-        <Square value = {squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value = {squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value = {squares[8]} onSquareClick={() => handleClick(8)}/>
+        {allSquares(6)}
       </div>
 
     </>
