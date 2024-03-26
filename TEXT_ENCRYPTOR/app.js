457b283036fa80a687902ccedd84262afb2861ba
German <germants100@gmail.com>
2024-03-26 09:58:24 -0600

    Getting the text form the textareas

--- TEXT_ENCRYPTOR/app.js
+++ TEXT_ENCRYPTOR/app.js
@@ -2,11 +2,15 @@ function display() {
     document.getElementById("img").style.display = "none";
 }
 
-
-const encriptar = () => {
+function change(){
     
 }
 
-const desencriptar = () => {
+const encriptar = () => {
+    let texto = document.getElementById('encriptado').value;
+    console.log(texto)
+}
 
+const desencriptar = () => {
+    let texto = document.getElementById('no-encriptado').value;
 }
