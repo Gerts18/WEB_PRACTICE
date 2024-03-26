3cc4ea83ae2298b6659ec09c258b6d005bdbbec6
German <germants100@gmail.com>
2024-03-26 10:13:07 -0600

    Encrypted completed

--- TEXT_ENCRYPTOR/app.js
+++ TEXT_ENCRYPTOR/app.js
@@ -2,13 +2,37 @@ function display() {
     document.getElementById("img").style.display = "none";
 }
 
-function change(){
-    
+let nuevoTexto = '';
+
+function change(letra){
+    switch(letra){
+        case 'e': 
+            nuevoTexto += 'enter'
+            break;
+        case 'i':
+            nuevoTexto += 'imes'
+            break;
+        case 'a':
+            nuevoTexto += 'ai'
+            break;
+        case 'o':
+            nuevoTexto += 'ober'
+            break;
+        case 'u':
+            nuevoTexto += 'ufat'
+            break;
+        default:
+            nuevoTexto += letra;
+            break;
+    }
 }
 
 const encriptar = () => {
     let texto = document.getElementById('encriptado').value;
-    console.log(texto)
+    let lista = Array.from(texto)
+    lista.forEach(element => change(element));
+    document.getElementById('no-encriptado').innerHTML = nuevoTexto;
+    display()
 }
 
 const desencriptar = () => {
