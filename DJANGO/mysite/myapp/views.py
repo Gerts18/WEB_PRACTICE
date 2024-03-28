247f2537d045801917c337db609086bc3c37381e
German <germants100@gmail.com>
2024-03-28 17:41:30 -0600

    Another mode to import a function

--- DJANGO/mysite/myapp/views.py
+++ DJANGO/mysite/myapp/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 # Create your views here.
 def message(request): # This will help me to obtain info if the client visits this view or ejecutes the function
-    return HttpResponse('<h1> Hello World <h1/>')
+    return HttpResponse('<h1> Hello World </h1>')
--- DJANGO/mysite/mysite/urls.py
+++ DJANGO/mysite/mysite/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import message
+""" from myapp.views import message """
+from myapp import views 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', message)
+    path('', views.hello)
 ]
