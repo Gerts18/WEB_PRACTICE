c460c5d7d04bc04d3ba1012264b3d991e84ea4ac
German <germants100@gmail.com>
2024-03-28 17:37:05 -0600

    First Hello world

--- DJANGO/mysite/myapp/views.py
+++ DJANGO/mysite/myapp/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def message(request): # This will help me to obtain info if the client visits this view or ejecutes the function
+    return HttpResponse('Hello World')
