8a2a6bc6be8186fc794a45e3f866a72b9a4997ff
German <germants100@gmail.com>
2024-03-28 17:37:54 -0600

    returning a html object

--- DJANGO/mysite/myapp/views.py
+++ DJANGO/mysite/myapp/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 # Create your views here.
 def message(request): # This will help me to obtain info if the client visits this view or ejecutes the function
-    return HttpResponse('Hello World')
+    return HttpResponse('<h1> Hello World <h1/>')
