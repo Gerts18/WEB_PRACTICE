c01fd0728aa785cb0aeedb776635f6371b84e0d1
German <germants100@gmail.com>
2024-03-31 13:29:53 -0600

    moving data from my back to the front

--- DJANGO/mysite/myapp/views.py
+++ DJANGO/mysite/myapp/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import get_object_or_404, render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    title = 'Welcome to Django Page!!'
+    return render(request, 'index.html', {
+        'title': title
+    })
 
 def about(request):
     return render(request, 'about.html')
