45cad4751849cf8dd3942536600e2a4ae31d9a65
German <germants100@gmail.com>
2024-03-30 19:29:17 -0600

    Learning how to get data form the navigator

--- DJANGO/mysite/myapp/urls.py
+++ DJANGO/mysite/myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.message),
-    path('info/',views.about)
+    path('info/',views.about),
+    path('message/<str:username>',views.message)
 ]
--- DJANGO/mysite/myapp/views.py
+++ DJANGO/mysite/myapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 # Create your views here.
-def message(request): # This will help me to obtain info if the client visits this view or ejecutes the function
+def message(request, username): # This will help me to obtain info if the client visits this view or ejecutes the function
+    print(username)
     return HttpResponse('<h1> Hello World </h1>')
 
 def about(request):
