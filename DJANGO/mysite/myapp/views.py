27e022a0e54ccd24ae84319c088332812524a2f2
German <germants100@gmail.com>
2024-03-30 19:33:51 -0600

    Using the data that i got form the navigator

--- DJANGO/mysite/myapp/views.py
+++ DJANGO/mysite/myapp/views.py
@@ -5,8 +5,7 @@ def index(request):
     return HttpResponse("Index Page")
 
 def message(request, username): # This will help me to obtain info if the client visits this view or ejecutes the function
-    print(username)
-    return HttpResponse(f'<h1> Hello World </h1>')
+    return HttpResponse(f'<h1> Hello World {username} </h1>')
 
 def about(request):
     return HttpResponse('About')
