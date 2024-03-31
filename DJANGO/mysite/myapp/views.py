05f295090ff5cee0d292d282a95b373504e98678
German <germants100@gmail.com>
2024-03-31 13:19:51 -0600

    Using render to show a static page

--- DJANGO/mysite/myapp/views.py
+++ DJANGO/mysite/myapp/views.py
@@ -1,16 +1,16 @@
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index Page")
-
-def message(request, username): # This will help me to obtain info if the client visits this view or ejecutes the function
-    return HttpResponse(f'<h1> Hello World {username} </h1>')
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse('About')
+
+def message(request, username): # This will help me to obtain info if the client visits this view or ejecutes the function
+    return HttpResponse(f'<h1> Hello World {username} </h1>')
 
 def projects(request):
     list_projects = list(Project.objects.values())
