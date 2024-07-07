ca3ff8cc8850fda942d20b3b2a934eaccd4a171b
German <germants100@gmail.com>
2024-07-06 21:25:51 -0600

    Using useState

--- REACT/org/src/components/MiOrg/index.js
+++ REACT/org/src/components/MiOrg/index.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './MiOrg.css'
 
 const MiOrg = () => {
+  //Estado - hooks
+  //useState
+  const [display, setDisplay] = useState(true);
+
+  const handleClick = () => {
+      setDisplay(!display);
+      console.log('Mostrar/ocultar elemento', display)
+  }
+
   return (
     <section className='orgSection'>
         <h3 className='title'>Mi organización</h3>
-        <img src='/img/add.png' alt='add'></img>
+        <img src='/img/add.png' alt='add' onClick={handleClick}/>
     </section>
   )
 }
