6a02f24d365f40466264aa85654b64894cf70184
German <germants100@gmail.com>
2024-07-07 10:21:52 -0600

    Converting input component to controlled component

--- REACT/org/src/components/Input/index.js
+++ REACT/org/src/components/Input/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import {React, useState} from 'react';
 import './Input.css';
 
 function Input(props) {
-
   //console.log('Datos:', props)
+
+  const [value, setValue] = useState("");
+
+  const handleValue = (event) => {
+    setValue(event.target.value);
+  }
 
   return (
     <div className='text-input'>
       <label>{props.titulo} </label>
-      <input placeholder={`Ingresa tu ${props.titulo.toLowerCase()}`} required={props.required} />
+      <input 
+        placeholder={`Ingresa tu ${props.titulo.toLowerCase()}`} 
+        required={props.required} 
+        value={value} 
+        onChange={handleValue}
+      />
     </div>
   )
 }
