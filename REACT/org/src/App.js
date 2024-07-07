a535515cfaf413351655236ce30cde17794e07c8
German <germants100@gmail.com>
2024-07-07 13:28:20 -0600

    Creating Team component

--- REACT/org/src/App.js
+++ REACT/org/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Form from './components/Form/Form.js';
 import Header from './components/Header/Header.jsx';
 import MiOrg from './components/MiOrg/index.js';
+import Team from './components/Team/index.js';
 
 /* console.log(Header) */
 
@@ -11,18 +12,14 @@ function App() {
   //Ternario -->  condicion ? true : false
   // corto circuito --> condicion && true
 
-  const [displayForm, setDisplayForm] = useState(true);
-
-  /* const changeDisplay = () => {
-    setDisplayForm(!displayForm);
-  } */
+  const [displayForm, setDisplayForm] = useState(false);
 
   return (
     <div>
       <Header/>
-      {/* displayForm ? <Form/> : <></> */}
       {displayForm && <Form/> }
       <MiOrg changeDisplay={() => setDisplayForm(!displayForm)} />
+      <Team/>  
     </div>
   )
 }
---
+++ REACT/org/src/components/Team/index.js
@@ -0,0 +1,14 @@
+import './Team.css';
+
+const Team = (props) => {
+    return (
+        <section className="team" >
+            <h3>Programación</h3>
+            <div className="collaborators">
+
+            </div>
+        </section>
+    )
+}
+
+export default Team
