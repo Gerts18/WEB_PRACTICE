04357ef3daf9361016736f2e58f1fb9e4ce7cc91
German <germants100@gmail.com>
2024-07-09 17:33:47 -0600

    Best way to do it with react-router-dom

--- REACT/react-router/src/App.js
+++ REACT/react-router/src/App.js
@@ -3,23 +3,18 @@ import './assets/css/componentes/card.css'
 import Home from './pages/Home.jsx'
 import Sobre from './pages/Sobre.jsx'
 
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 function App() {
 
-  const location = window.location.pathname
-  console.log(location)
-
-  const Router = () => {
-    if (location === '/'){
-      return <Home/>
-    }else {
-      return <Sobre/>
-    }
-  }
-
+  
   return (
-    <>
-      {Router()}
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sobre' element={<Sobre/>} />
+      </Routes>
+    </Router>
   );
 }
 
