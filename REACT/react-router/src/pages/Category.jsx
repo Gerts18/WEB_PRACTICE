a5f969099ff2fe74652e5349438e1c42ab7b31f5
German <germants100@gmail.com>
2024-07-10 11:17:55 -0600

    Creting subcategories with useResolvedPath

--- REACT/react-router/src/pages/Category.jsx
+++ REACT/react-router/src/pages/Category.jsx
@@ -3,11 +3,19 @@ import '../assets/css/blog.css'
 import { buscar } from "../api/api";
 import ListCategories from "../components/ListCategories";
 import ListPost from "../components/ListPost";
-import { useParams, Routes, Route } from "react-router-dom";
+import { useParams, Routes, Route, Link, useResolvedPath } from "react-router-dom";
 
 const Category = () => {
 
+  const [subCategorias, setSubCategorias] = useState([])
+
   const {id} = useParams()
+
+  const {url} = useResolvedPath("").pathname
+
+  useEffect(() => {
+    buscar(`/categorias?id=${id}`,(response) => {setSubCategorias(response[0].subcategorias)} )
+  }, [id])
 
   return (
     <>
@@ -15,6 +23,19 @@ const Category = () => {
         <h2 className="title-page"> Pet Noticias </h2>
       </div>
       <ListCategories/>
+      <ul className="category-list container flex">
+        {
+          subCategorias.map((subcategoria, index) => {
+            return (
+              <li className={`category-list__category category-list__category--${id}`} key={index}>
+                <Link to={`${url}/${subcategoria}`} >
+                  {subcategoria}
+                </Link>
+              </li>
+            )
+          })
+        }
+      </ul>
       <Routes>
         <Route path="/" element={<ListPost url={`/posts?categoria=${id}`} />} />
       </Routes>
