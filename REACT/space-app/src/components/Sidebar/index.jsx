ada40b997bd4805621bbcb5918c80999b83b1241
German <germants100@gmail.com>
2024-07-11 12:55:50 -0600

    Adding more items to my sidebar

--- REACT/space-app/src/components/Sidebar/index.jsx
+++ REACT/space-app/src/components/Sidebar/index.jsx
@@ -20,12 +20,43 @@ const Sidebar = () => {
         <aside>
             <nav>
                 <ListalEstilizada>
-                    <NavigationItem iconoActivo="iconos/home-activo.png" iconoInactivo="iconos/home-inactivo.png" activo={iconoActivo} change={handleActive} >
-                        Inicio
+
+                    <NavigationItem 
+                        iconoActivo="iconos/home-activo.png" 
+                        iconoInactivo="iconos/home-inactivo.png" 
+                        activo={iconoActivo} 
+                        change={handleActive} >
+                            Inicio
                     </NavigationItem>
-                    <NavigationItem iconoActivo="iconos/mas-vistas-activo.png" iconoInactivo="iconos/mas-vistas-inactivo.png" activo={!iconoActivo} change={handleActive} >
-                        Más visitados
+                    <NavigationItem 
+                        iconoActivo="iconos/mas-vistas-activo.png" 
+                        iconoInactivo="iconos/mas-vistas-inactivo.png" 
+                        activo={!iconoActivo} 
+                        change={handleActive} >
+                            Más visitados
                     </NavigationItem>
+                    <NavigationItem
+                        iconoActivo="/iconos/me-gusta-activo.png"
+                        iconoInactivo="/iconos/me-gusta-inactivo.png"
+                        activo={!iconoActivo} 
+                        change={handleActive} >
+                            Más Me Gusta
+                    </NavigationItem>
+                    <NavigationItem
+                        iconoActivo="/iconos/nuevas-activo.png"
+                        iconoInactivo="/iconos/nuevas-inactivo.png"
+                        activo={!iconoActivo} 
+                        change={handleActive} >
+                            Nuevas
+                    </NavigationItem>
+                    <NavigationItem
+                        iconoActivo="/iconos/sorprendeme-activo.png"
+                        iconoInactivo="/iconos/sorprendeme-inactivo.png"
+                        activo={!iconoActivo} 
+                        change={handleActive}>
+                        Sorpréndeme
+                    </NavigationItem>
+
                 </ListalEstilizada>
             </nav>
         </aside>
