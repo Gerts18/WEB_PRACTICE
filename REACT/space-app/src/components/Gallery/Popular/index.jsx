9029f63ed582bd36d54f6b3022985565ce0a0190
German <germants100@gmail.com>
2024-07-12 13:56:09 -0600

    Styling Popular component and adding images to it

--- REACT/space-app/src/components/Gallery/Popular/index.jsx
+++ REACT/space-app/src/components/Gallery/Popular/index.jsx
@@ -1,12 +1,34 @@
+import styled from "styled-components"
 import Title from "../../Title"
+import populares from './fotos-populares.json'
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  & > img {
+    max-width: 212px;
+    height: 158px;
+    border-radius: 20px;
+  }
+
+`
 
 
 const Popular = () => {
   return (
-    <>
+    <section>
         <Title $align={"center"} >Populares</Title>
-    </>
+        <Container>
+          {
+            populares.map( (popular) => {
+              return <img key={popular.id} src={popular.path} alt={popular.alt} />
+            } )
+          }
+        </Container>
+        
+    </section>
   )
 }
 
