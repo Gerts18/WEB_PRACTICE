20709d8692624c6bc49631d021fee1206de163b5
German <germants100@gmail.com>
2024-07-16 13:32:45 -0600

    Using fetch with params

--- REACT/alura-cinema/src/pages/Player/index.jsx
+++ REACT/alura-cinema/src/pages/Player/index.jsx
@@ -2,14 +2,19 @@ import Banner from 'components/Banner'
 import styles from './Player.module.css'
 import Titulo from 'components/Titulo'
 import { useParams } from 'react-router-dom'
-import videos from "data/db.json"
 import NotFound from 'pages/NotFound'
 
 const Player = () => {
 
+    const [video, setVideo] = useState([])
+
     const parametros = useParams()
 
-    const video = videos.find(video => video.id === Number(parametros.id))
+    useEffect( () => {
+        fetch(`https://my-json-server.typicode.com/Gerts18/api-videos/videos?id=${parametros.id}`)
+        .then(response => response.json())
+        .then(data => setVideo(data))
+    }, []);
 
     if(!video) return <NotFound/>
 
