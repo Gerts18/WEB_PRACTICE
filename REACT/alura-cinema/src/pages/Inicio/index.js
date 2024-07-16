bad481f63934403abad0989593f5cd58b09a9fd0
German <germants100@gmail.com>
2024-07-15 18:26:59 -0600

    Configuring my routes

---
+++ REACT/alura-cinema/src/pages/Inicio/index.js
@@ -0,0 +1,9 @@
+
+
+const Inicio = () => {
+  return (
+    <h1>Hola mundo desde el Inicio</h1>
+  )
+}
+
+export default Inicio
---
+++ REACT/alura-cinema/src/routes.js
@@ -0,0 +1,15 @@
+import Inicio from "./pages/Inicio"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Inicio/>} />
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes
