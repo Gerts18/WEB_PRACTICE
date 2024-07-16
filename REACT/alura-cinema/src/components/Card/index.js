49a045a23dcda1fd50c045b0e9edf3f405377cd6
German <germants100@gmail.com>
2024-07-16 11:29:58 -0600

    Implementing my custom hook

--- REACT/alura-cinema/src/components/Card/index.js
+++ REACT/alura-cinema/src/components/Card/index.js
@@ -1,13 +1,21 @@
+import { useFavoritosContext } from 'context/Favoritos'
 import styles from './Card.module.css'
 import iconNoFavorito from './favorite-empty.png'
 import iconFavorito from './favorite-full.png'
 
 const Card = ({id, capa, titulo}) => {
+
+  const {favorito, agregarFavorito} = useFavoritosContext()
+
+  const isFavorito = favorito.some(fav => fav.id === id)
+
+  const icon = isFavorito ? iconFavorito : iconNoFavorito
+
   return (
     <div className={styles.container} >
         <img src={capa} alt={titulo} className={styles.capa} />
         <h2>{titulo} </h2>
-        <img src={iconNoFavorito} alt='Icono favorito'  />
+        <img src={icon} alt='Icono favorito' onClick={() => agregarFavorito({id, titulo, capa})} />
     </div>
   )
 }
