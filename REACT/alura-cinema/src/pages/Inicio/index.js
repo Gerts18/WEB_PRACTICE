41e3f1309961a9add028490997375397ea625f0d
German <germants100@gmail.com>
2024-07-16 13:26:14 -0600

    Using json-server and useEffect, consuming an API

--- REACT/alura-cinema/src/pages/Inicio/index.js
+++ REACT/alura-cinema/src/pages/Inicio/index.js
@@ -1,22 +1,31 @@
 import Banner from "components/Banner"
 import Card from "components/Card"
 import Titulo from "components/Titulo"
-import videos from "data/db.json"
 import styles from './Inicio.module.css'
+import { useEffect, useState } from "react"
 
 const Inicio = () => {
+
+  const [videos, setVideos] = useState([])
+
+  useEffect( () => {
+     fetch("https://my-json-server.typicode.com/Gerts18/api-videos/videos")
+    .then(response => response.json())
+    .then(data => setVideos(data))
+  }, []);
+
   return (
     <>
       <Banner img='home' color='#154580' />
       <Titulo>
         <h1>Un lugar para guardar sus videos favoritos </h1>
       </Titulo>
-        
+
       <section className={styles.container}>
         {
-          videos.map( (video) => {
+          videos.map((video) => {
             return <Card {...video} key={video.id} />
-          } )
+          })
         }
       </section>
     </>
