c5083aa11f13be4b6697d0b97d5fc4c66d139ed3
German <germants100@gmail.com>
2024-07-16 11:19:58 -0600

    Creating a custom hook to manage the state of my favorites

--- REACT/alura-cinema/src/context/Favoritos.jsx
+++ REACT/alura-cinema/src/context/Favoritos.jsx
@@ -1,4 +1,4 @@
-const { createContext, useState } = require("react");
+const { createContext, useState, useContext } = require("react");
 
 
 export const FavoritosContext = createContext()
@@ -10,11 +10,37 @@ const FavoritosProvider = ({ children }) => {
     const [favorito, setFavorito] = useState([])
 
     return (
-        <FavoritosContext.Provider value={{favorito, setFavorito}}>
+        <FavoritosContext.Provider value={{ favorito, setFavorito }}>
             {children}
         </FavoritosContext.Provider>
     )
 }
 
 export default FavoritosProvider
+
+
+
+export function useFavoritosContext() {
+    const { favorito, setFavorito } = useContext(FavoritosContext);
+
+    function agregarFavorito(nuevoFavorito) {
+        const favoritoRepetido = favorito.some(
+            item => item.id === nuevoFavorito.id
+        );
+
+        let nuevaLista = [...favorito]
+        if (!favoritoRepetido) {
+            nuevaLista.push(nuevoFavorito)
+            return setFavorito(nuevaLista)
+        }
+
+        nuevaLista = favorito.filter(
+            item => item.id !== nuevoFavorito.id
+        );
+        return setFavorito(nuevaLista)
+    }
+
+    return { favorito, agregarFavorito }
+}
+
 
